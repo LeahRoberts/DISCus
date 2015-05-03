3bb59a87fbf73cc993ec766f06388f4f82b3193d
Leah <lrobe09@gmail.com>
2015-05-03 13:08:11 +1000

    coordinates generation added

--- CIRQUE_create_reference.py
+++ CIRQUE_create_reference.py
@@ -120,9 +120,6 @@ os.remove("new_file.fa")
 
 #### Part 3: create coordinates file (if not using hyxR or fimS inversion regions)
 
-with open(name + "-coordinates.txt", "w") as f:
-        f.write("Region" + "\t" + "Start" + "\t" + "End" + "\n")
-
 A_left_flank = 1000
 A_switch_A = 1001 
 A_switch_B = 1000 + int(inv_length)
@@ -135,6 +132,7 @@ B_switch_B = B_left_B + inv_length
 B_right_A = int(B_switch_B) + 1
 
 with open (name + "-coordinates.txt", "w") as f:
+	f.write("Region" + "\t" + "Start" + "\t" + "End" + "\n")
 	f.write("A_left_flank" + "\t" + "n/a" + "\t" + str(A_left_flank) + "\n")
 	f.write("A_switch_region" + "\t" + str(A_switch_A) + "\t" + str(A_switch_B) + "\n")
 	f.write("A_right_flank" + "\t" + str(A_right_A) + "\t" + str(A_right_B) + "\n")
